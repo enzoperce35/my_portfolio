9ac1080bd5de7a031657b0ec6846b01c569d4ccd
Edgar Lorenz G. Castro <elgc@Edgars-iMac.local>
2024-06-23 12:20:42 +0800

    made my logo

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import Logo from './logo'
+import '../style/App.css'
+
+function App() {
+
+  return (
+    <div id='container'>
+      <Logo />
+    </div>
+  )
+}
+
+export default App
---
+++ src/app/logo.jsx
@@ -0,0 +1,10 @@
+export default function Logo() {
+  
+  return (
+    <div id='logo'>
+      <span className='bars' id='top-bar'></span>
+      <span className='bars' id='mid-bar'></span>
+      <span className='bars' id='btm-bar'></span>
+    </div>
+  )
+}
